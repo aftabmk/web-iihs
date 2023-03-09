61993c3e4231c1dfb53172f90d12d9914fe98db1
Aftabmk <kizhisey@gmail.com>
2023-03-09 20:35:44 +0530

    new file:   wiki/api.json 	modified:   wiki/function.js 	modified:   wiki/index.js 	modified:   wiki/package-lock.json 	modified:   wiki/package.json

--- wiki/index.js
+++ wiki/index.js
@@ -1,14 +1,14 @@
-import { getRank , getUrl } from './function.js'
+import express from "express"
+import { scrape } from './function.js'
 
-const url = 'https://en.wikipedia.org/wiki/Nifty_Fifty_(disambiguation)'
-const getData = async(url)=>{
-    let data = []
-    const arr = await getUrl(url)
-    for (let url of arr ){
-        let rank = await getRank(`https://en.wikipedia.org/w/index.php?title=${url.split('/').at(-1)}&action=info`)
-        data.push({url,rank})
-        console.log(rank)
-    }
-    console.log(data)
-}
-getData(url)
+// const url = "https://en.wikipedia.org/wiki/Redux_(JavaScript_library)";
+const app = express();
+const PORT = 4000
+
+app.get('/search?',async(req,res)=>{
+    const url = req.query.url
+    const data = await scrape(url)
+    res.send({data:data})
+})
+
+app.listen(PORT,()=>console.log(`Running on http://localhost:${PORT}`))

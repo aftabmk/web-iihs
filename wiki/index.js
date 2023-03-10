f6be587c7742a6156bce87404c8657282567ffe7
Aftabmk <kizhisey@gmail.com>
2023-03-10 08:54:33 +0530

    modified:   wiki/index.js

--- wiki/index.js
+++ wiki/index.js
@@ -8,7 +8,8 @@ const PORT = 4000
 app.get('/search?',async(req,res)=>{
     const url = req.query.url
     const data = await scrape(url)
-    res.send({data:data})
+    const result = data.map(element=>{if(element.rank){return element}}).filter(element=>element).sort((a,b)=>b.rank-a.rank)
+    res.send({data:result})
 })
 
 app.listen(PORT,()=>console.log(`Running on http://localhost:${PORT}`))
